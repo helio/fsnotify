f29508cc21fa6ed8ef585408d69607a065367c59
Michael Weibel <michael@helio.exchange>
2025-03-24 14:04:39 +0100

    feat: windows USN implementation

--- backend_windows_test.go
+++ backend_windows_test.go
@@ -1,4 +1,4 @@
-//go:build windows
+//go:build windows && !usn
 
 package fsnotify
 
@@ -6,11 +6,15 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+
+	"go.uber.org/goleak"
 )
 
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
+
 func TestRemoveState(t *testing.T) {
-	// TODO: the Windows backend is too confusing; needs some serious attention.
-	return
 
 	var (
 		tmp  = t.TempDir()
